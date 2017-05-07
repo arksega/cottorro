692ecf8e94d04107c50db8118997d04781db7c40
Sergio Gutierrez Alvarez <ark.sega@gmail.com>
2017-05-07 01:50:51 -0500

    Show existing tweets

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,25 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model import Tweet
+import jinja2
 import falcon
+
+with open('www/base.html') as fd:
+    template = jinja2.Template(fd.read())
+
+engine = create_engine('sqlite:///cottorro.db')
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
 
 
 class Home(object):
 
     def on_get(self, req, resp):
         resp.content_type = 'text/html'
-        with open('www/base.html') as fd:
-            resp.body = fd.read()
+        print(session.query(Tweet).all())
+        resp.body = template.render({'tweets': session.query(Tweet).all()})
+
 
 app = application = falcon.API()
 app.add_route('/', Home())
