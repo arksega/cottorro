9502544fdd6d05de1a17fe43ab4b5ac1a83bfcd5
Sergio Gutierrez Alvarez <ark.sega@gmail.com>
2017-05-06 21:44:47 -0500

    Adding html

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,9 @@ import falcon
 class Home(object):
 
     def on_get(self, req, resp):
-        resp.body = 'Hola mundo Web!'
+        resp.content_type = 'text/html'
+        with open('www/base.html') as fd:
+            resp.body = fd.read()
 
 app = application = falcon.API()
 app.add_route('/', Home())
