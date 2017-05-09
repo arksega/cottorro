f5d75c3542f6e7b87366d9bf371a1e2757fa134c
Sergio Gutierrez Alvarez <ark.sega@gmail.com>
2017-05-09 05:49:02 -0500

    Creation of users and logging check

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,13 @@ from jinja2 import Environment, FileSystemLoader
 from beaker.middleware import SessionMiddleware
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from collections import namedtuple
 from model import Tweet, User
+from base64 import b64encode
+import sqlalchemy
+import hashlib
 import falcon
+import os
 
 j2_env = Environment(loader=FileSystemLoader('www/'), trim_blocks=True)
 
@@ -12,12 +17,15 @@ Session = sessionmaker()
 Session.configure(bind=engine)
 db = Session()
 
+Alert = namedtuple('Alert', ('level', 'title', 'text'))
+
 
 class AuthMiddleware(object):
 
     def process_request(self, req, resp):
+        anonym = ('/login', '/', '/signup')
         session = req.env['beaker.session']
-        if not session.get('logged_in', None) and req.path != '/login':
+        if not session.get('logged_in', None) and req.path not in anonym:
             raise falcon.HTTPFound('/login')
 
 
@@ -48,10 +56,20 @@ class Login(object):
     def on_post(self, req, resp):
         user = db.query(User).filter(User.id == req.params['username']).first()
         if user:
-            session = req.env['beaker.session']
-            session['logged_in'] = user.id
-            session.save()
-            raise falcon.HTTPFound('/')
+            sha256 = hashlib.sha256()
+            sha256.update((user.salt + req.params['password']).encode())
+            key = sha256.hexdigest()
+            if key == user.key:
+                session = req.env['beaker.session']
+                session['logged_in'] = user.id
+                session.save()
+                raise falcon.HTTPFound('/')
+            else:
+                resp.content_type = 'text/html'
+                ermsg = 'Usuario o contraseña invalidos'
+                data = {'alerts': [Alert('urgent', '', ermsg)]}
+                tmpl = j2_env.get_template('login.html')
+                resp.body = tmpl.render(data)
         else:
             self.on_get(req, resp)
 
@@ -63,6 +81,44 @@ class Logout(object):
         session['logged_in'] = None
         session.save()
         raise falcon.HTTPFound('/')
+
+
+class Signup(object):
+
+    def _error(self, req, resp, msg):
+            data = {
+                'alerts': [Alert('urgent', '', msg)],
+                'username': req.params['username'],
+                'email': req.params['email']
+            }
+            resp.content_type = 'text/html'
+            tmpl = j2_env.get_template('signup.html')
+            resp.body = tmpl.render(data)
+
+    def on_get(self, req, resp):
+        resp.content_type = 'text/html'
+        tmpl = j2_env.get_template('signup.html')
+        resp.body = tmpl.render()
+
+    def on_post(self, req, resp):
+        if req.params['password'] != req.params['password2']:
+            ermsg = 'Ambos campos de contraseña deben coincidir'
+            self._error(req, resp, ermsg)
+            return
+        salt = b64encode(os.urandom(18)).decode()
+        sha256 = hashlib.sha256()
+        sha256.update((salt + req.params['password']).encode())
+        key = sha256.hexdigest()
+        db.add(User(id=req.params['username'], salt=salt, key=key))
+        try:
+            db.commit()
+            session = req.env['beaker.session']
+            session['logged_in'] = req.params['username']
+            session.save()
+            raise falcon.HTTPFound('/')
+        except sqlalchemy.exc.IntegrityError:
+            ermsg = 'El usuarion {} ya existe'.format(req.params['username'])
+            self._error(req, resp, ermsg)
 
 
 class TweetResource(object):
@@ -88,6 +144,7 @@ app.add_route('/', Home())
 app.add_route('/login', Login())
 app.add_route('/logout', Logout())
 app.add_route('/write', TweetResource())
+app.add_route('/signup', Signup())
 # Configure the SessionMiddleware
 session_opts = {
     'session.type': 'file',
