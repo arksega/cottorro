2f76174a14f4858a82262d4ee53f8b0a83edc26e
Sergio Gutierrez Alvarez <ark.sega@gmail.com>
2017-05-08 21:14:15 -0500

    Session without verification

--- wsgi.py
+++ wsgi.py
@@ -1,25 +1,61 @@
+from jinja2 import Environment, FileSystemLoader
+from beaker.middleware import SessionMiddleware
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from model import Tweet
-import jinja2
+from model import Tweet, User
 import falcon
 
-with open('www/base.html') as fd:
-    template = jinja2.Template(fd.read())
+j2_env = Environment(loader=FileSystemLoader('www/'), trim_blocks=True)
 
 engine = create_engine('sqlite:///cottorro.db')
 Session = sessionmaker()
 Session.configure(bind=engine)
-session = Session()
+db = Session()
 
 
 class Home(object):
 
     def on_get(self, req, resp):
         resp.content_type = 'text/html'
-        print(session.query(Tweet).all())
-        resp.body = template.render({'tweets': session.query(Tweet).all()})
+        session = req.env['beaker.session']
+        if 'logged_in' in session:
+            print(db.query(Tweet).all())
+            tmpl = j2_env.get_template('home.html')
+            resp.body = tmpl.render({'tweets': db.query(Tweet).all()})
+        else:
+            raise falcon.HTTPMovedPermanently('/login')
 
 
-app = application = falcon.API()
+class Login(object):
+
+    def on_get(self, req, resp):
+        resp.content_type = 'text/html'
+        session = req.env['beaker.session']
+        if 'logged_in' in session:
+            raise falcon.HTTPMovedPermanently('/')
+        else:
+            tmpl = j2_env.get_template('login.html')
+            resp.body = tmpl.render()
+
+    def on_post(self, req, resp):
+        user = db.query(User).filter(User.id == req.params['username']).first()
+        if user:
+            session = req.env['beaker.session']
+            session['logged_in'] = user.id
+            session.save()
+            raise falcon.HTTPMovedPermanently('/')
+        else:
+            self.on_get(req, resp)
+
+
+app = falcon.API()
+app.req_options.auto_parse_form_urlencoded = True
 app.add_route('/', Home())
+app.add_route('/login', Login())
+# Configure the SessionMiddleware
+session_opts = {
+    'session.type': 'file',
+    'session.data_dir': 'sessions',
+    'session.cookie_expires': True,
+}
+application = SessionMiddleware(app, session_opts)
